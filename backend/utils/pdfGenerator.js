096383949a2818bcb9612aeb9dc316ad523d63fc
Kushak Zohaad Jafry <66268795+kushakjafry@users.noreply.github.com>
2022-11-13 23:38:30 +0530

    Update pdfGenerator.js

--- backend/utils/pdfGenerator.js
+++ backend/utils/pdfGenerator.js
@@ -1,7 +1,23 @@
 const puppeteer = require("puppeteer");
 const path = require("path");
+const PCR = require("puppeteer-chromium-resolver");
 const generatePdf = async (html) => {
-  const browser = await puppeteer.launch();
+    const option = {
+        revision: "",
+        detectionPath: "",
+        folderName: ".chromium-browser-snapshots",
+        defaultHosts: ["https://storage.googleapis.com", "https://npm.taobao.org/mirrors"],
+        hosts: [],
+        cacheRevisions: 2,
+        retry: 3,
+        silent: false
+    };
+    const stats = await PCR(option);
+  const browser = await stats.puppeteer.launch({
+        headless: false,
+        args: ["--no-sandbox"],
+        executablePath: stats.executablePath
+  });
   const page = await browser.newPage();
   await page.setContent(html);
   await page.addStyleTag({
